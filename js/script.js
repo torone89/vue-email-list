1421d440a0d920a69a20c5147d7d9da595bf3190
torone89 <torone89>
2022-07-20 13:11:11 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+// VERIFICA COLLEGAMENTO JAVA
+console.log("JS")
+
+// VERIFICA COLLEGAMENTO VUEJS
+
+console.log('Vue ok', Vue)
+
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+new Vue({
+    el: '#root  ',
+    data: {
+    },
+
+    methods: {
+    },
+
+});
+
+
+
+
