0e42794e2a8254d261a4588c57b5a9fd69ac4f44
torone89 <torone89>
2022-07-20 13:46:18 +0200

    Esercizio Base Completato

--- js/script.js
+++ js/script.js
@@ -6,21 +6,34 @@ console.log("JS")
 
 console.log('Vue ok', Vue)
 
-// Descrizione:
-// Attraverso l'apposita API di Boolean
-// https://flynn.boolean.careers/exercises/api/random/mail
-// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 new Vue({
     el: '#root  ',
     data: {
+        mailList: [],
     },
-
     methods: {
+        generateTenMail: function () {
+            // Attraverso l'apposita API di Boolean
+            // https://flynn.boolean.careers/exercises/api/random/mail
+            // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
+                    .then((res) => {
+                        const result = res.data.response;
+                        this.mailList.push(result)
+
+                    });
+            };
+        },
     },
 
+    created() {
+        this.generateTenMail();
+    }
 });
 
 
